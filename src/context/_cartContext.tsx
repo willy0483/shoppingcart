0cc3e81b5129d757ed8500555aa0e12674dba945
willy0483 <willyjensen251@gmail.com>
2025-05-02 08:48:58 +0200

    add

--- src/context/_cartContext.tsx
+++ src/context/_cartContext.tsx
@@ -3,8 +3,14 @@ import { ProductElement } from "../types/product.types";
 
 export const CartContext = createContext({
   cartData: [] as Array<ProductElement>,
-  setCartData: (productArr: ProductElement[]) => {},
-  addToCart: (productID: ProductElement) => {},
-  removeFromCart: (productID: number) => {},
+  setCartData: (productArr: ProductElement[]) => {
+    console.log("productArr", productArr);
+  },
+  addToCart: (productID: ProductElement) => {
+    console.log("productID", productID);
+  },
+  removeFromCart: (productID: number) => {
+    console.log("productID", productID);
+  },
   clearCart: () => {},
 });
